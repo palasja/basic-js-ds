f63ad3c882f3c473cc006255faea05ee02f79db1
palasja <palasja@gmail.com>
2022-05-02 00:03:46 +0300

    feat: queue

--- src/queue.js
+++ src/queue.js
@@ -1,6 +1,6 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require("../extensions/list-node.js");
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -13,24 +13,67 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
-class Queue {
+// class Queue extends ListNode {
+//   constructor() {
+//     super();
+//   }
+//   arr = [];
+//   getUnderlyingList() {
+//     let next = this.arr.length > 1 ? this.arr[1] : null;
+//     let obj = {};
+//     let prev = null;
+//     this.arr.forEach((el) => {
+//       let tmp = { value: el, next: null };
+//       if (obj.value == null) {
+//         obj = tmp;
+//       } else {
+//         prev.next = tmp;
+//       }
+//       prev = tmp;
+//     });
+//     return obj;
+//   }
+//   s;
+//   enqueue(value) {
+//     this.arr.push(value);
+//   }
 
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+//   dequeue() {
+//     let tmp = this.arr[0];
+//     this.arr.shift();
+//     return tmp;
+//   }
+// }
+
+class Queue extends ListNode {
+  constructor() {
+    super();
   }
-
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  list = {};
+  getUnderlyingList() {
+    return this.list;
+  }
+  s;
+  enqueue(value) {
+    const obj = { value: value, next: null };
+    if ("value" in this.list) {
+      let tmp = this.list;
+      while (tmp.next != null) {
+        tmp = tmp.next;
+      }
+      tmp.next = obj;
+    } else {
+      this.list = obj;
+    }
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let tmp = this.list.value;
+    this.list = this.list.next;
+    return tmp;
   }
 }
 
 module.exports = {
-  Queue
+  Queue,
 };
